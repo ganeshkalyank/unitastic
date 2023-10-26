fa7bff66f9d60d00ef87fc65c476de24b9fc2a10
ganeshkalyank <srikgk333@gmail.com>
2023-10-26 23:01:18 +0530

    added maintenance page

---
+++ src/pages/Maintenance.jsx
@@ -0,0 +1,18 @@
+import notfound from "../assets/notfound.svg"
+import "./Maintenance.css"
+
+const Maintenance = () => {
+    return (
+        <>
+            <div className="container maintenance-container">
+                <h1 className="maintenance-title text-center">Unitastic</h1>
+                <div className="d-flex justify-content-center">
+                    <img src={notfound} className="maintenance-img" alt="Not Found" />
+                </div>
+                <h1 className="maintenance-title text-center">Under Maintenance!</h1>
+            </div>
+        </>
+    );
+}
+
+export default Maintenance;
